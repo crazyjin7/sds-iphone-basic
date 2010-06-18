480a0d2c7e5e9c46aec58471911f228d7a9ba282
crazyjin7 <crazyjin7@2263c9a6-f23e-8a61-b916-c54c7822d073>
2010-06-18 00:18:40 +0000

    WorldPhotos - Detail View

---
+++ WorldPhotos/Classes/PhotoDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  PhotoDetailViewController.h
+//  WorldPhotos
+//
+//  Created by SDS06 on 10. 6. 18..
+//  Copyright 2010 sds_multicampus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PhotoDetailViewController : UIViewController {
+
+}
+
+@end
