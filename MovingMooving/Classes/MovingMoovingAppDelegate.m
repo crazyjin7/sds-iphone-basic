9d287892b59c5c3962faf15a3029de6312e1ce49
crazyjin7 <crazyjin7@2263c9a6-f23e-8a61-b916-c54c7822d073>
2010-06-18 07:36:41 +0000

    MovingMooving (5)

--- MovingMooving/Classes/MovingMoovingAppDelegate.m
+++ MovingMooving/Classes/MovingMoovingAppDelegate.m
@@ -31,19 +31,19 @@
 
 - (IBAction)move:(id)sender {
 	[UIView beginAnimations:@"moving" context:nil];
-	[UIView setAnimationDuration:2];
+
+	//애니메이션 속도 조절
+	[UIView setAnimationDuration:0.5];
 	
 	//애니메이션 좀더 부드럽게...
 	[UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
 	
 	movingView.center = CGPointMake(250, 300);
+	movingView.backgroundColor = [UIColor blueColor];
+	movingView.alpha = 0.7;
 	movingView.transform = CGAffineTransformMakeRotation(90.0);
 	
 	[UIView commitAnimations];
-	
-	//commit 후에는 애니메이션 효과 없음
-	movingView.backgroundColor = [UIColor blueColor];
-	movingView.alpha = 0.7;
 }
 
 
