8c62d0653ed0eb6fa796c20ba27a3c51baaa3f43
crazyjin7 <crazyjin7@2263c9a6-f23e-8a61-b916-c54c7822d073>
2010-06-18 02:36:16 +0000

    SimpleSketch (2)

---
+++ SimpleSketch/Classes/CanvasView.h
@@ -0,0 +1,16 @@
+//
+//  CanvasView.h
+//  SimpleSketch
+//
+//  Created by SDS06 on 10. 6. 18..
+//  Copyright 2010 sds_multicampus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CanvasView : UIView {
+
+}
+
+@end
---
+++ SimpleSketch/Classes/CanvasView.m
@@ -0,0 +1,35 @@
+//
+//  CanvasView.m
+//  SimpleSketch
+//
+//  Created by SDS06 on 10. 6. 18..
+//  Copyright 2010 sds_multicampus. All rights reserved.
+//
+
+#import "CanvasView.h"
+
+
+@implementation CanvasView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
