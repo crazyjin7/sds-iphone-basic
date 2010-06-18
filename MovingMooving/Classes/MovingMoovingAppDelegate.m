f7bc48d595b8ce8c2be2278ddd128572768cc46b
crazyjin7 <crazyjin7@2263c9a6-f23e-8a61-b916-c54c7822d073>
2010-06-18 07:30:19 +0000

    MovingMooving (3)

--- MovingMooving/Classes/MovingMoovingAppDelegate.m
+++ MovingMooving/Classes/MovingMoovingAppDelegate.m
@@ -33,6 +33,9 @@
 	[UIView beginAnimations:@"moving" context:nil];
 	[UIView setAnimationDuration:2];
 	
+	//애니메이션 좀더 부드럽게...
+	[UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+	
 	movingView.center = CGPointMake(250, 300);
 	movingView.backgroundColor = [UIColor blueColor];
 	movingView.transform = CGAffineTransformMakeRotation(90.0);
